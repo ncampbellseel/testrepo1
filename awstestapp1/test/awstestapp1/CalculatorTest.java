2937064203b3e9d577f05e771220094ffd3e8b58
ncampbell <ncampbell@ITEM-S31506.dhcp.edin.uk.sopra>
2016-10-27 19:56:17 +0100

    Moved test class to test structure.
    Maven build failed because test class was not in awstestapp1/test
    structure within project

--- awstestapp1/test/awstestapp1/CalculatorTest.java
+++ awstestapp1/test/awstestapp1/CalculatorTest.java
@@ -2,15 +2,16 @@ package awstestapp1;
 
 import static org.junit.Assert.*;
 
-import org.junit.Test;
+import org.junit.*;
 
 public class CalculatorTest {
-
+	
 	@Test
 	public void testEvaluate() {
 	    Calculator calculator = new Calculator();
 	    int sum = calculator.evaluate("1+2+3");
 	    assertEquals(6, sum);
 	}
+
 
 }
